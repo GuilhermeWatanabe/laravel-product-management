cbfba96a2dfcaf70fe9185ef49f54acc7c46847e
Guilherme Watanabe <watanab430@gmail.com>
2025-10-20 01:20:11 -0300

    fix: conflict with route names.

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,14 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProductAPIController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::name('api.')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::apiResource('/products', ProductAPIController::class);
+    Route::name('api.')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::apiResource('/products', ProductAPIController::class);
+    });
 });
